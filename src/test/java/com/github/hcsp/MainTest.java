664471d3175c14ae9da7d6905cbf11abc68277cd
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:06 +0800

    Update repo

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -1,10 +1,8 @@
 package com.github.hcsp;
 
-import static org.hamcrest.Matchers.containsStringIgnoringCase;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import com.github.blindpirate.extensions.CaptureSystemOutput;
 import java.lang.reflect.Field;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
@@ -19,6 +17,6 @@ public class MainTest {
     public void hasMessageMember() throws Exception {
         Field field = Main.class.getDeclaredField("message");
         field.setAccessible(true);
-        assertEquals(field.get(null).toString().toLowerCase(), "hello");
+        Assertions.assertEquals(field.get(null).toString().toLowerCase(), "hello");
     }
 }
