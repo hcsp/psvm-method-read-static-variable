aff77619b2afbaa114306ae9a78fbb59145f8d3e
XinleZhou <2320789321>
2020-03-06 18:15:35 +0800

    push1

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,9 +3,9 @@ package com.github.hcsp;
 public class Main {
 
     // 在这里创建一个名为"message"的静态成员变量，并存储字符串"Hello"
-    public static String message;
+    public static String message="Hello";
 
     public static void main(String[] args) {
-        Main.message = "Hello";
+        System.out.println(message);
     }
 }
