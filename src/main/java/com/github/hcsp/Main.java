638be2209c887722086a7fbce84491de393e7d2d
bycgxss <bycgxss@gmail.com>
2020-10-13 15:13:30 +0800

    创建静态成员变量message

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,6 +3,7 @@ package com.github.hcsp;
 public class Main {
     // Create a static member "message" here to store the string "Hello"
     // 在这里创建一个名为"message"的静态成员变量，并存储字符串"Hello"
+    public static String message = "Hello";
 
     public static void main(String[] args) {
         System.out.println(message);
