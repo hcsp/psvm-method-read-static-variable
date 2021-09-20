d0fb0b7c8703bf5ba7b27a81efea1d771e542d12
YJL <2567843704@qq.com>
2021-09-20 13:09:28 +0800

    2

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,11 +1,8 @@
 package com.github.hcsp;
-
-import com.sun.org.apache.xpath.internal.objects.XString;
-
 public class Main {
     // Create a static member "message" here to store the string "Hello"
     // 在这里创建一个名为"message"的静态成员变量，并存储字符串"Hello"
-  static String messsage = "Hello";
+  public static String message="Hello";
     public static void main(String[] args) {
         System.out.println("message");
     }
